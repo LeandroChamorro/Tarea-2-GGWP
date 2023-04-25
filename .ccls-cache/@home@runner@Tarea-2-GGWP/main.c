4dac333c31fda63c26cdbbcb0fe5d8251e5946ae
LeandroChamorro <127540706+LeandroChamorro@users.noreply.github.com>
2023-04-25 23:39:51 +0000

    Librerías, estructuras y main
    +Se incluyeron las librerías.
    +Se declararon las estructuras a utilizar que son:
    -TipoJugador almacena los datos del mismo.
    -TipoAcción se utilizará para la pilas de acciones del jugador.
    -tipoItemCont se utilizará en el mapa de items.
    +Se crea la función main la cual crea una lista de jugadores y un mapa que contendrá los items y los jugadores que lo poseen.

--- .ccls-cache/@home@runner@Tarea-2-GGWP/main.c
+++ .ccls-cache/@home@runner@Tarea-2-GGWP/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+#include "Map.h"
+#include "Stack.h"
+
 
 int main(void) {
-  printf("Hello World\n");
+  List *jugadores = createList();
+  Map * mapaItems = createMap(is_equal_string);
   return 0;
 }
