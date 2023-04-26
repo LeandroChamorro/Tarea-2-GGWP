a77102edfaacb84f805021af7fe0de9bdc2d6729
LeandroChamorro <127540706+LeandroChamorro@users.noreply.github.com>
2023-04-26 00:30:30 +0000

    Avance 2- Leandro Chamorro
    +Se agregó la función menu la cual muestra en pantalla un menú y llama a las funciones correspondientes según lo que elija el usuario.
    -Las funciones serán agregadas mas adelante.
    -Se agregó la llamada del menú al main.

--- main.c
+++ main.c
@@ -6,9 +6,84 @@
 #include "Map.h"
 #include "Stack.h"
 
+//Menú principal
+void menu(List *jugadores,Map*mapaItems){
+  //Se crea una variable "opcion" la cual será una condicionante para el ciclo "while" base de nuestro programa
+  int opcion = 1;
+  while(opcion != 0){
+    printf("╔════════════════════════════•°🜧°•════════════════════════════╗\n");
+    printf("║ Si desea crear el perfil de un jugador presione 1           ║\n");
+    printf("║ Si desea mostrar datos de un jugador presione 2             ║\n");
+    printf("║ Si desea agregar un item de un jugador presione 3           ║\n");
+    printf("║ Si desea eliminar un item de un jugador presione 4          ║\n");
+    printf("║ Si desea agregar puntos de habilidad a un jugador presione 5║\n");
+    printf("║ Si desea mostrar todos los jugadores con un item en especifico presione 6║\n");
+    printf("║ Si desea deshacer la última acción de un jugador presione 7 ║\n");
+    printf("║ Si desea exportar datos de jugadores presione 8             ║\n");
+    printf("║ Si desea importar datos de jugadores presione 9             ║\n");
+    printf("║ Si no desea hacer nada más presione 0                       ║\n");
+    printf("╚════════════════════════════•°🜥°•════════════════════════════╝\n\n");
+    //Se cambia el valor de la variable "opcion" con un valor que desee el usuario realizar
+    scanf("%d", &opcion);
+    getchar();
+    //Se utiliza un switch para acceder a las opciones de cada función
+    switch(opcion){
+      case 1: //insertarJugador(jugadores);
+      break; 
+            
+      case 2: //mostrarJugador(jugadores);
+      break;
+          
+      case 3: //procesoInsertarItem(jugadores,mapaItems);
+      break;
+
+      case 4: //EliminarItem(jugadores, mapaItems);
+      break;
+      
+      case 5: //agregarPh(jugadores);
+      break;
+        
+      case 6: //ItemEspecifico(jugadores,mapaItems);
+      break;
+
+      case 7: //deshacerAcción(jugadores,mapaItems);
+      break;
+    
+      case 8: //importarDatos(jugadores,mapaItems);
+      break;
+
+      case 9: //exportarDatos(jugadores,mapaItems);
+      break;
+      //en caso de ser cero se imprime lo sgte. Para finalizar el programa
+      case 0:
+        printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀\n");
+        printf("⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀\n");
+        printf("⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄\n");
+        printf("⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄\n");
+        printf("⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄\n");
+        printf("⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷\n");
+        printf("⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿\n");
+        printf("⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃\n");
+        printf("⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇\n");
+        printf("⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧\n");
+        printf("⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿\n");
+        printf("⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿\n");
+        printf("⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇\n");
+        printf("⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃\n");
+        printf("⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶ ⢠⣿⣿\n");
+        printf("⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇\n");
+        printf("⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇\n");
+        printf("⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏\n");
+        printf("⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁\n");
+        printf("          Adiós by GG WP//");
+        break;
+    }
+  }
+}
 
 int main(void) {
   List *jugadores = createList();
   Map * mapaItems = createMap(is_equal_string);
+  menu(jugadores,mapaItems);
   return 0;
 }
